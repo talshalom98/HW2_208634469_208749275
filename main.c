aab96a02f14d057c7ffdadc9b2aa6002e258e0ae
Matan Shiloni <mshiloni22@gmail.com>
2021-05-03 07:36:56 +0300

    I think I finished float_neg

--- main.c
+++ main.c
@@ -10,11 +10,11 @@ int main()
     /*int x = ;
     int y = -9999999999999999999;
     int ok = add_ok(x, y);
-    printf("%d", ok);*/
+    printf("%d", ok);
     printf("Enter 32-bit number, representing a floating point:\n");
     unsigned int uf;
     scanf("%x", &uf);
-    printf("%x\n", float_neg(uf));
+    printf("%x\n", float_neg(uf)); */
     return 0;
 }
 
@@ -32,7 +32,8 @@ unsigned float_neg (unsigned uf)
 {
     unsigned int xor_element = (1 << 31);   //1000 0000 0000 ... integer. for '^' bitwise operator with uf.
     int frac_mask = (1U << 23) - 1;  //to check if frac > 0
-    unsigned exp_only = uf >> 23;
-    int exp_mask = (1U << 8) -1;
+    unsigned exp_only = uf >> 23; //to check if exp = 111...
+    if (((frac_mask & uf) > 0) && (exp_only == 0x000000ff))
+        return uf;
     return uf ^ xor_element;
 }
