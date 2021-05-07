6bcf26fdf35dbc51f91c7bb9c5d3414c59cff58f
Matan Shiloni <mshiloni22@gmail.com>
2021-05-07 10:40:30 +0300

    final check

--- main.c
+++ main.c
@@ -33,7 +33,8 @@ unsigned float_neg (unsigned uf)
     unsigned int xor_element = (1 << 31);   //1000 0000 0000 ... integer. for '^' bitwise operator with uf.
     int frac_mask = (1U << 23) - 1;  //to check if frac > 0
     unsigned exp_only = uf >> 23; //to check if exp = 111...
-    if (((frac_mask & uf) > 0) && (exp_only == 0x000000ff))
+    if (((frac_mask & uf) > 0) && (exp_only == 0x000000ff)) { //check if NAN
         return uf;
+    }
     return uf ^ xor_element;
 }
